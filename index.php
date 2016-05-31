d894785294f1bd5212c71d443b657164b39a9140
Marc Perel <marc@obox.co.za>
2016-05-31 13:53:15 +0200

    Updated readme.txt

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	Plugin URI: http://oboxthemes.com
 	Description: Deploys a beautiful, responsive Coming Soon or Down for Maintenance page for your site. Collect emails, change styles, show social links and more
 	Author: Obox Themes
-	Version: 1.0.9
+	Version: 1.0.10
 	Author URI: http://oboxthemes.com
 */
 
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: obox
 Tags: maintenance mode, landing page, splash screen
 Requires at least: 3.3
-Tested up to: 4.4.1
-Stable tag: 1.0.9
+Tested up to: 4.5.2
+Stable tag: 1.0.10
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
